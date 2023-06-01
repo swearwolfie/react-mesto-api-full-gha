39bb0bb3bedc9ba19e472210708195427b7a1e81
Masha Soldatova <threebadwolves@gmail.com>
2023-06-02 02:53:52 +0300

    apiii6

--- backend/app.js
+++ backend/app.js
@@ -102,7 +102,7 @@ app.use(errors()); // обработчик ошибок celebrate
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res
-    .status(err.statusCode)
+    .status(statusCode)
     .send({
       message: statusCode === 500
         ? 'Произошла ошибка на сервере'

bf59525e43e28eacfd69c2deb0531ba42ac3da9d
Masha Soldatova <threebadwolves@gmail.com>
2023-06-03 19:38:02 +0300

    apiiiii9

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -11,7 +11,7 @@ const {
 router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
-    link: Joi.string().regex(/^(https?:\/\/(www\.)?([a-zA-z0-9-]){1,}\.?([a-zA-z0-9]){2,8}(\/?([a-zA-z0-9-])*\/?)*\/?([-._~:/?#[]@!\$&'\(\)\*\+,;=])*)/).required,
+    link: Joi.string().regex(/^(https?:\/\/(www\.)?([a-zA-z0-9-]){1,}\.?([a-zA-z0-9]){2,8}(\/?([a-zA-z0-9-])*\/?)*\/?([-._~:/?#[]@!\$&'\(\)\*\+,;=])*)/).required(),
   }),
 }), createCard);
 router.get('/', getCards);

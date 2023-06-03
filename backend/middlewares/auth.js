9c739bcf4da9d4b2c8d0a6981b18a941186d4830
Masha Soldatova <threebadwolves@gmail.com>
2023-06-03 18:47:39 +0300

    apiiiii6

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,26 +1,24 @@
 // middlewares/auth.js
-const jwt = require('jsonwebtoken');
+const token = require('jsonwebtoken');
 const { AuthError } = require('../utils/constants');
+const UnauthorizedError = require('../errors/unauth-err')
 const { JWT_SECRET } = require('../config');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res
-      .status(AuthError)
-      .send({ message: 'Необходима авторизация' });
+    return next(new UnauthorizedError('Необходима авторизация'));
   }
 
   const jwt = authorization.replace('Bearer ', '');
   let payload;
 
   try {
-    payload = jwt.verify(jwt, JWT_SECRET);
+    payload = token.verify(jwt, JWT_SECRET);
+  //  req.user = payload;
   } catch (err) {
-    return res
-      .status(AuthError)
-      .send({ message: 'Необходима авторизация' });
+    return next(new UnauthorizedError('Необходима авторизация'));
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса

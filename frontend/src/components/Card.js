04a6c8ff7833f1a65e39234d9ea0dc7eee4dc9de
Masha Soldatova <threebadwolves@gmail.com>
2023-06-06 19:56:41 +0300

    apiccсc4

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -9,6 +9,7 @@ function Card({ card, onCardClick, onCardLike, onCardDelete }) {
   const isOwn = card.owner._id === currentUser._id;
   console.log(card, 'do you believe in life after love');
   console.log(currentUser, 'i can feel something inside me say')
+  console.log(card.owner, 'i really dont think im strong enough now');
   const isLiked = card.likes.some(i => i._id === currentUser._id);
   const cardLikeButtonClassName = `cards__like-button ${
     isLiked && "cards__like-button_active "

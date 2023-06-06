03772678420d4a35fcaef572258ca6341762e332
Masha Soldatova <threebadwolves@gmail.com>
2023-06-06 19:55:07 +0300

    apiccсc3

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -7,6 +7,8 @@ function Card({ card, onCardClick, onCardLike, onCardDelete }) {
   }
   const currentUser = React.useContext(CurrentUserContext);
   const isOwn = card.owner._id === currentUser._id;
+  console.log(card, 'do you believe in life after love');
+  console.log(currentUser, 'i can feel something inside me say')
   const isLiked = card.likes.some(i => i._id === currentUser._id);
   const cardLikeButtonClassName = `cards__like-button ${
     isLiked && "cards__like-button_active "

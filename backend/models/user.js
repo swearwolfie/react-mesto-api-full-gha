8fc03b46c4e691e0f461716722a98795627681a9
Masha Soldatova <threebadwolves@gmail.com>
2023-06-07 01:25:32 +0300

    apip3

--- backend/models/user.js
+++ backend/models/user.js
@@ -18,12 +18,12 @@ const userSchema = new mongoose.Schema({
   },
   avatar: {
     type: String, // ссылка — это строка
-    validate: {
+/*    validate: {
       validator(v) {
         return /^(https?:\/\/(www\.)?([a-zA-z0-9-]{1}[a-zA-z0-9-]*\.?)*\.{1}([a-zA-z0-9]){2,8}(\/?([a-zA-z0-9-])*\/?)*\/?([-._~:?#[]@!\$&'\(\)\*\+,;=])*)/.test(v);
       },
       message: 'Некорректная ссылка',
-    },
+    }, */
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
   email: {

675123ad932a7e7cd08fec32079c9b09e59bbb63
Masha Soldatova <threebadwolves@gmail.com>
2023-06-08 01:46:28 +0300

    feat: add crash-test

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const { JWT_SECRET = 'JWT_SECRET' } = process.env;
+const { JWT_SECRET = 'some-secret-key' } = process.env;
 const { DB_ADDRESS = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 module.exports = {

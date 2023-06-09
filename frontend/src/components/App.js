3f1de9ade128b36e644857eec7d943678c294ca5
Masha Soldatova <threebadwolves@gmail.com>
2023-06-09 23:07:25 +0300

    feat: add crash-test1

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -141,9 +141,7 @@ function App() {
 
   function handleCardLike(card) {
     // Снова проверяем, есть ли уже лайк на этой карточке
-    const isLiked = card.likes.some((i) => i._id === currentUser._id);
-    console.log(card.likes, 'help ');
-    console.log(card, 'help me');
+    const isLiked = card.likes.some(i => i === currentUser._id);
 
     // Отправляем запрос в API и получаем обновлённые данные карточки
     apiThingie

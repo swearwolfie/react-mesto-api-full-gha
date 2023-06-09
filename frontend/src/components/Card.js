6d7279065a7356a96a02c1a33501e2475363b274
Masha Soldatova <threebadwolves@gmail.com>
2023-06-09 23:11:11 +0300

    feat: add crash-test

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -8,7 +8,7 @@ function Card({ card, onCardClick, onCardLike, onCardDelete }) {
   const currentUser = React.useContext(CurrentUserContext);
   const isOwn = card.owner._id === currentUser._id;
   console.log(card, card.likes, 'bambaleo')
-  const isLiked = card.likes.some(i => i === currentUser._id);  
+  const isLiked = card.likes.some((i) => i._id === currentUser._id);  
   const cardLikeButtonClassName = `cards__like-button ${
     isLiked && 'cards__like-button_active'
   }`;
